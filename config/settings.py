3d3eebb5d908e078356bace95cade20849d81fb8
davisweredev <davisweredev@gmail.com>
2025-11-06 20:45:33 +0300

    updated settings.py to include www subdomain in CORS and CSRF trusted origins`

--- config/settings.py
+++ config/settings.py
@@ -36,8 +36,6 @@ INSTALLED_APPS = [
     'apps.core',
     'apps.school',
     'apps.transactions',
-
-
 ]
 
 
@@ -53,6 +51,7 @@ CSP_FRAME_ANCESTORS = ("'self'",
                         "http://localhost:5173",
                         "http://localhost:3000",
                         "https://studybuddy.africa",
+                        "https://www.studybuddy.africa",
                         "http://localhost:5174")
 
 
@@ -63,6 +62,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:5174',
     "https://studybuddy.africa",
+    "https://www.studybuddy.africa",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -80,7 +80,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:5174',
     "https://studybuddy.africa",
-
+    'https://www.studybuddy.africa',
 ]
 
 
