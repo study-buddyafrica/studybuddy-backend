f21b746fbbd3f9982e848b5661352f3827831f64
davisweredev <davisweredev@gmail.com>
2025-11-13 14:31:00 +0300

    `Added Topic and Subtopic models, updated serializers and admin.py to reflect changes`

--- apps/school/admin.py
+++ apps/school/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from apps.school.models import (
     SessionBooking, Subject,
-    Grade,Course, 
-    LiveSession
+    Grade,Course, Subtopic,
+    LiveSession,Topic
 )
 
 admin.site.register(SessionBooking)
 admin.site.register(Grade)
 admin.site.register(Course)
 admin.site.register(Subject)
+admin.site.register(Topic)
+admin.site.register(Subtopic)
 admin.site.register(LiveSession)
