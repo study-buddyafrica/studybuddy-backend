a87c204d5d21ce37904c1ccac0caaf775e0236d0
davisweredev <davisweredev@gmail.com>
2025-11-13 13:19:04 +0300

    remove subject teacher

--- apps/school/models.py
+++ apps/school/models.py
@@ -35,8 +35,6 @@ class Subject(Core):
     code = models.CharField(max_length=30, unique=True, null=True, blank=True) 
     name = models.CharField(max_length=150, db_index=True) 
     description = models.TextField(blank=True, null=True) 
-    teachers = models.ManyToManyField( 'users.TeacherProfile', related_name="school_subjects") 
-    
     
     def __str__(self): return f"{self.name} ({self.code or 'N/A'})" 
     
