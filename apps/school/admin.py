2bfc4e101ec49bda2d5a00aacc53de1ca5dc9a05
davisweredev <davisweredev@gmail.com>
2025-11-13 13:16:15 +0300

    `Added Subject model to admin.py`

--- apps/school/admin.py
+++ apps/school/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from apps.school.models import SessionBooking, Grade,Course, LiveSession
+from apps.school.models import (
+    SessionBooking, Subject,
+    Grade,Course, 
+    LiveSession
+)
 
 admin.site.register(SessionBooking)
 admin.site.register(Grade)
 admin.site.register(Course)
+admin.site.register(Subject)
 admin.site.register(LiveSession)
