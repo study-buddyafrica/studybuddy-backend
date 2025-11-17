3435d82918ec36f211f19b9afcb7184faf550cdb
davisweredev <davisweredev@gmail.com>
2025-11-17 15:24:02 +0300

    `Remove user parameter from EmailVerificationCode delete method`

--- apps/users/serializers/create_user_profile_serializer.py
+++ apps/users/serializers/create_user_profile_serializer.py
@@ -55,7 +55,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         EmailVerificationCode.objects.filter(
             email=email, 
             user__isnull=True
-        ).delete(user=user)
+        ).delete()
 
         role = user.role
         if role == "teacher":
