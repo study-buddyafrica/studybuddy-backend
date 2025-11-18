c7344b91df7fc9b9f770110d266b7c6c97b5f45a
davisweredev <davisweredev@gmail.com>
2025-11-18 09:48:42 +0300

    `Reordered and updated static/media settings`

--- config/settings.py
+++ config/settings.py
@@ -208,14 +208,13 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 
