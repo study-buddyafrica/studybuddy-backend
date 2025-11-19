dc3b8435a841c8c509e34a4f19a8ab66112b20d1
davisweredev <davisweredev@gmail.com>
2025-11-19 12:51:08 +0300

    `Updated config/settings.py with changes to SECRET_KEY, DEBUG, and ALLOWED_HOSTS`

--- config/settings.py
+++ config/settings.py
@@ -10,12 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY =os.getenv("SECRET_KEY")
 
-
 DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
 
 ALLOWED_HOSTS = ["backend.studybuddy.africa",'54.225.241.26','127.0.0.1']
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
