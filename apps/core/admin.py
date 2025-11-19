23324aa0f596647d6c2ff5f9c2266f4c6690a5ef
davisweredev <davisweredev@gmail.com>
2025-11-19 15:01:25 +0300

    uncomment

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,6 +1,6 @@
-# from django.contrib import admin
-# from django.contrib.auth.admin import UserAdmin
-# from .models import User, EmailVerificationCode
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, EmailVerificationCode
 
 # # class CustomUserAdmin(UserAdmin):
 # #     """Custom admin configuration for User model"""
