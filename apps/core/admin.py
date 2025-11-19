96b85e447b4fc7c9566ab45e7d16ce0e8c53067a
davisweredev <davisweredev@gmail.com>
2025-11-19 10:32:35 +0300

    `Updated CustomUserAdmin class in admin.py to reflect changes in list_display and list_filter`

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -5,20 +5,45 @@ from .models import User, EmailVerificationCode
 class CustomUserAdmin(UserAdmin):
     """Custom admin configuration for User model"""
     
-    list_display = ('account_confirmed','email', 'first_name', 'last_name', 'role', 'is_staff', 'is_active', 'created_at')
-    list_filter = ('role', 'is_staff', 'is_active', 'created_at','account_confirmed')
+    list_display = (
+        'account_confirmed',
+        'email', 'first_name', 
+        'last_name', 'role', 
+        'is_staff', 'is_active', 
+        'created_at'
+    )
+    list_filter = (
+        'role', 'is_staff', 
+        'is_active', 'created_at',
+        'account_confirmed'
+        'country'
+    )
     
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {'fields': ('first_name', 'last_name', 'username','account_confirmed')}),
-        ('Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important Dates', {'fields': ('last_login', 'created_at', 'updated_at')}),
+        ('Personal Info', 
+        {'fields': (
+            'first_name', 'last_name', 
+            'username','account_confirmed', 
+            'country'
+        )}),
+        ('Permissions', 
+        {'fields': (
+            'role', 'is_active', 
+            'is_staff', 'is_superuser', 
+            'groups', 'user_permissions'
+        )}),
+        ('Important Dates', 
+        {'fields': 
+        ('last_login', 'created_at', 
+          'updated_at'
+        )}),
     )
 
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'first_name', 'last_name', 'username', 'password1', 'password2', 'role', 'is_active', 'is_staff','account_confirmed')}
+            'fields': ('email', 'first_name', 'last_name', 'username', 'password1', 'password2', 'role', 'is_active', 'is_staff','account_confirmed', 'country')}
         ),
     )
     
