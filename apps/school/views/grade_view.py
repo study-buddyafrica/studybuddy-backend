6900326fad301bedd84b633c250fa867410c40a8
davisweredev <davisweredev@gmail.com>
2025-12-22 10:00:07 +0300

    `Disable create, update, partial_update and destroy methods for GradeViewSet`

--- apps/school/views/grade_view.py
+++ apps/school/views/grade_view.py
@@ -1,5 +1,5 @@
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 
 from apps.school.serializers.grade_serializer import GradeSerializer
 from apps.school.models import Grade
@@ -38,4 +38,42 @@ class GradeViewSet(viewsets.ModelViewSet):
 
         return Grade.objects.filter(teacher_grades=user.teacher_profile).order_by(
             "level"
+        )
+
+    def create(self, request, *args, **kwargs):
+        """
+        Disable create method - grades are created automatically
+        """
+        return Response(
+            {"detail": 'Method "POST" not allowed. Grades are created automatically.'},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
+
+    def update(self, request, *args, **kwargs):
+        """
+        Disable update method - grades are created automatically
+        """
+        return Response(
+            {"detail": 'Method "PUT" not allowed. Grades are created automatically.'},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
+
+    def partial_update(self, request, *args, **kwargs):
+        """
+        Disable partial update method - grades are created automatically
+        """
+        return Response(
+            {"detail": 'Method "PATCH" not allowed. Grades are created automatically.'},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        """
+        Disable delete method - grades are created automatically
+        """
+        return Response(
+            {
+                "detail": 'Method "DELETE" not allowed. Grades are created automatically.'
+            },
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
         )
