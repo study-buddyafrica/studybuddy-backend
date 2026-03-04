d24ebb87562a6fdeb94b55b9d4920d955613ae0e
DavisDev <davisweredev@gmail.com>
2026-03-04 15:43:00 +0300

    Update urls.py

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -18,6 +18,10 @@ urlpatterns = [
         TokenRefreshView.as_view(), 
         name="token_refresh"
     ),
+    path("token/request/",
+         CustomObtainTokenPairView.as_view(), 
+         name="token_request"
+        ),
     path(
         "verify-email/request/", 
         RequestRegistrationVerificationCode.as_view(), 
